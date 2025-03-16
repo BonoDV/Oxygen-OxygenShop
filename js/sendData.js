8777f9a1f8016e6756d27e7748f52ff614790505
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-03-16 14:37:38 +0100

    Send data from testing API improved

--- js/sendData.js
+++ js/sendData.js
@@ -1,18 +1,18 @@
 async function sendDataToAPI() {
-
+    let messageSent = document.getElementById("section__contact__div__form__sentStatus");
     try {
         // Recopilar datos del formulario
-        const nameValue = document.forms["contactForm"]["name"].value;
-        const emailValue = document.forms["contactForm"]["email"].value;
-        const checkBoxValue = document.forms["contactForm"]["checkBox"].checked;
+        let nameForm = document.getElementById("name");
+        let emailForm = document.getElementById("email");
+        let checkBoxForm = document.getElementById("section__contact__div__form__checkBox");
 
         // Enviar datos a la API
         const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
             body: JSON.stringify({
-                name: nameValue,
-                email: emailValue,
-                checked: checkBoxValue,
+                name: nameForm,
+                email: emailForm,
+                checked: checkBoxForm,
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
@@ -21,13 +21,21 @@ async function sendDataToAPI() {
 
         // Verificar si la respuesta es exitosa (código 200-299)
         if (!response.ok) {
+            messageSent.style.color = "red";
+            messageSent.textContent = 'Error. Please try again.'
             throw new Error(`Error en la solicitud: ${response.status} ${response.statusText}`);
+
         } else {
-            alert(`Datos enviados`);
+            messageSent.style.color = "green";
+            messageSent.textContent = 'Data sent succesfully.'
+            nameForm.value = "";
+            emailForm.value = "";
+            checkBoxForm.checked = false;
         }
 
     } catch (error) {
         // Capturar y mostrar errores
-        alert(`Hubo un error: ${error.message}. Inténtelo de nuevo.`);
+        messageSent.textContent = 'Error. Please try again.'
+        messageSent.style.color = "red";
     }
 }
